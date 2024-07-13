b433b735b4846e9bc6497a4fc5366f5c3982695c
Olamide Olatunde <victorolamide04@gmail.com>
2024-07-13 15:22:01 +0100

    investment:oage

---
+++ components/SmartInvesting.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function SmartInvesting() {
+  return (
+    <div className="max-w-screen-xl mx-auto w-full mt-20">
+      <div className="flex items-center flex-col justify-center text-center">
+        <div>
+          <h1 className="text-[14px] text-[#3C76E1] font-switzer font-[500]">
+            Investment
+          </h1>
+          <h1 className="text-[31px] text-[#262626] capitalize font-[500] font-switzer">
+            Smart Investing
+          </h1>
+          <h1 className="text-[31px] text-[#262626] capitalize font-[500] font-switzer -mt-4">
+            With Rich Aunty Finance
+          </h1>
+        </div>
+          <p className="text-[#595959] font-[400] text-[13px] font-switzer w-[50%] text-center mt-4">
+            We handle all the complexities of investing for you. Our team of
+            experts manages your investment, making informed decisions to
+            maximize your returns while minimizing risk. You don't need to worry
+            about trading or market analysis we do it all for you.
+          </p>
+      </div>
+    </div>
+  );
+}
