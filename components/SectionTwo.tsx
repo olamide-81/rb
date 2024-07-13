91e2db4e2568b395f71a77479bee6d8604c04d0a
Olamide Olatunde <victorolamide04@gmail.com>
2024-07-13 13:09:09 +0100

    section:two

--- components/SectionTwo.tsx
+++ components/SectionTwo.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import rafopportunities from "@/assets/rafopportunities.svg"
+import appleanalysis from "@/assets/appleanalysis.svg"
+import rafanalysis from "@/assets/rafanalysis.svg"
+import creditscore from "@/assets/creditscore.svg"
 
 export default function SectionTwo() {
   return (
-    <div>SectionTwo</div>
-  )
+    <div className="max-w-screen-xl mx-auto w-full mt-20">
+      <div className="bg-[#F1F5F9] rounded-2xl py-10 px-20 flex items-center justify-between">
+        <div>
+            <div>
+                <Image src={rafopportunities} alt="asset-ab"/>
+            </div>
+            <div className="mt-10">
+                <Image src={appleanalysis} alt="asset-apple"/>
+            </div>
+        </div>
+        <div></div>
+        <div>
+            <div>
+                <Image src={rafanalysis} alt="asset-ab"/>
+            </div>
+            <div className="mt-10">
+                <Image src={creditscore} alt="asset-apple"/>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
 }
