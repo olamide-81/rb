36001e33d551a35285ad4053af6edb34c8a5dcd1
george-hub331 <idiaghegeorge9@gmail.com>
2024-07-13 10:40:08 +0100

    font change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
 
+const mcomic = localFont({
+  src: [
+    {
+  
+      path: "../public/font/medium.ttf",
+      weight: "500",
+      style: "medium",
+    },
+    {
+      path: "../public/font/regular.ttf",
+      weight: "400",
+      style: "regular",
+    },
+    {
+      path: "../public/font/bold.ttf",
+      weight: "600",
+      style: "bold",
+    },
+  ],
+  variable: "--font-mc",
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+ return <main className={`${mcomic.variable} font-sans`}>
+    <Component {...pageProps} />;
+  </main>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        mc: ["var(--font-mc)"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
