9d2c956b7ef1fba53bd9f20fb18e20ca17bb901f
george-hub331 <idiaghegeorge9@gmail.com>
2024-07-13 12:36:06 +0100

    minor font change

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen font-mc flex-col font-[400] items-center justify-between p-24`}
+      className={`flex min-h-screen font-mc flex-col font-[600] items-center justify-between font-switzer p-24`}
     >
       Hello
     </main>
