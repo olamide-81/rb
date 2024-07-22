f6f52c1c37bb42bc2f0b5baa28e2d7ce87316697
george-hub331 <idiaghegeorge9@gmail.com>
2024-07-22 21:48:14 +0100

    update: add favicon and meta images

--- lib/sanity.api.ts
+++ lib/sanity.api.ts
@@ -5,15 +5,9 @@ export const useCdn = false
  * Importing other npm packages here could lead to needlessly increasing the client bundle size, or end up in a server-only function that don't need it.
  */
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET',
-)
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production"
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID',
-)
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 
 export const readToken = process.env.SANITY_API_READ_TOKEN || ''
 
